5158e60fc969a617e97199a3c60b3a6d0d35ddfd
cicxcoin <cicxcoin@gmail.com>
2019-11-05 01:39:35 +0700

    Implement x11

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -7,10 +7,14 @@
 
 #include <hash.h>
 #include <tinyformat.h>
+#include <streams.h>
 
 uint256 CBlockHeader::GetHash() const
 {
-    return SerializeHash(*this);
+    std::vector<unsigned char> vch(80);
+    CVectorWriter ss(SER_NETWORK, PROTOCOL_VERSION, vch, 0);
+    ss << *this;
+    return HashX11((const char *)vch.data(), (const char *)vch.data() + vch.size());
 }
 
 std::string CBlock::ToString() const
