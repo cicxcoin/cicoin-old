f9b4d6b51321df7abe14ac018f29e80a2367d7ba
cicxcoin <cicxcoin@gmail.com>
2019-11-05 01:19:16 +0700

    Fix CMakeFiles

--- CMakeFiles/build-cicoin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/build-cicoin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/build-cicoin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
